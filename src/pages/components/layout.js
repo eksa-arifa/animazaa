3c59e1cfdff21c0dbef1f9799a83940133edf4cf
Eksa <arifamuhammad1536@gmail.com>
2023-06-15 21:23:05 +0700

    Update

--- src/pages/components/layout.js
+++ src/pages/components/layout.js
@@ -1,6 +1,9 @@
 import Head from "next/head";
 import Navbar from "./navbar";
 import Stylist from "@/styles/stylist.module.css";
+import { Instagram } from "react-feather";
+import { GitHub } from "react-feather";
+import Link from "next/link";
 
 
 export default function Layout(props){
@@ -14,6 +17,13 @@ export default function Layout(props){
         <div style={{minHeight: 1000}} className={Stylist.body}>
         {props.children}
         </div>
+        <footer className={Stylist.footer}>
+            <div className={Stylist.socmed}>
+                <Link href={"https://instagram.com/eksrifaa"}><Instagram/></Link>
+                <Link href={"https://github.com/eksa-arifa"}><GitHub/></Link>
+            </div>
+            <div className={Stylist.foot}>Salam hangat kami Animazaa</div>
+        </footer>
         </>
     )
 }
